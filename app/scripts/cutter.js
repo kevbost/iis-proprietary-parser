2953de2a7a4907bc555eca84de2509f854450392
Kevin Bost <kevbost@gmail.com>
2015-10-30 15:33:50 -0400

    refactored node code into cutter.js, added json logs to repo, refactored criteria match into more specific regex system

--- app/scripts/cutter.js
+++ app/scripts/cutter.js
@@ -18,6 +18,10 @@ var data = [
 ]
 
 var rebuildArray = function() {
+    // _.without(arr, "#Fields:")
+    // remove "#Fields" from array to build dynamic IIS Key
+    // ====================================================
+
     var newData = [];
 
     for (var i = 1; i < data.length; i++) {
@@ -33,35 +37,14 @@ var rebuildArray = function() {
 
 var parseJSONforAudioReferences = function(line){
     // FILTERS DOWN AUDIOFILES AND REMOVES KNOWN BOTS
-    // ==============================================
-    var audiotypes = line.indexOf(".mp3") >= 0 || line.indexOf(".mp4") >= 0 || line.indexOf(".m4a") >= 0;
-    var bots = line.indexOf("Googlebot") >= 0 || line.indexOf("baidu") >= 0;
-
-    // ADD "&& COUNT <= NUMBER" TO THIS END BECAUSE 100,000+ LINE FILES
-    // ================================================================
-    if (audiotypes && !bots) {
-        // CREATE NEW ARRAY BY SPLITTING BY SPACES, PUSH TO DATA ARRAY
-        // ===========================================================
-        var arr = line.split(/[ ]+/);
-        matchCount +=1;
-        // ADD A UNIQUE ID TO EACH OBJECT, PUSHED TO THE END, MAPPING TO THE LAST KEY VALUE
-        // ================================================================================
-        arr.push('id_' + (__.uniqueId() - 1));
-        data.push(arr);
-    }
-}
-
-var convertEntireLogFileToJSONEliminatingHeaders = function(line){
-
-    // FILTERS DOWN AUDIOFILES AND REMOVES KNOWN BOTS
     // CHANGED TO LINE.MATCH REGEX FOR DRYNESS && EXTENDABLILITY
     // USED TO BE line.indexOf(".mp3") >= 0 || line.indexOf(".mp4") >= 0 || line.indexOf(".m4a") >= 0;
     // ===============================================================================================
     var audiotypes = line.match( /(\.mp3|\.m4a|\.aac|\.mp4|\.m4p|\.m4r|\.3gp|\.ogg|\.oga|\.wma|\.wav|\.flac)/ );
     var bots = line.match( /(googlebot|Googlebot-Mobile|Googlebot-Image|Mediapartners-Google|bingbot|slurp|java|wget|curl|Commons-HttpClient|Python-urllib|libwww|httpunit|nutch|phpcrawl|msnbot|jyxobot|FAST-WebCrawler|FAST Enterprise Crawler|biglotron|teoma|convera|seekbot|gigablast|exabot|ngbot|ia_archiver|GingerCrawler|webmon|httrack|webcrawler|grub.org|UsineNouvelleCrawler|antibot|netresearchserver|speedy|fluffy|bibnum.bnf|findlink|msrbot|panscient|yacybot|AISearchBot|IOI|ips-agent|tagoobot|MJ12bot|dotbot|woriobot|yanga|buzzbot|mlbot|yandexbot|purebot|Linguee Bot|Voyager|CyberPatrol|voilabot|baiduspider|citeseerxbot|spbot|twengabot|postrank|turnitinbot|scribdbot|page2rss|sitebot|linkdex|Adidxbot|blekkobot|ezooms|dotbot|Mail.RU_Bot|discobot|heritrix|findthatfile|europarchive.org|NerdByNature.Bot|sistrix crawler|ahrefsbot|Aboundex|domaincrawler|wbsearchbot|summify|ccbot|edisterbot|seznambot|ec2linkfinder|gslfbot|aihitbot|intelium_bot|facebookexternalhit|yeti|RetrevoPageAnalyzer|lb-spider|sogou|lssbot|careerbot|wotbox|wocbot|ichiro|DuckDuckBot|lssrocketcrawler|drupact|webcompanycrawler|acoonbot|openindexspider|gnam gnam spider|web-archive-net.com.bot|backlinkcrawler|coccoc|integromedb|content crawler spider|toplistbot|seokicks-robot|it2media-domain-crawler|ip-web-crawler.com|siteexplorer.info|elisabot|proximic|changedetection|blexbot|arabot|WeSEE:Search|niki-bot|CrystalSemanticsBot|rogerbot|360Spider|psbot|InterfaxScanBot|Lipperhey SEO Service|CC Metadata Scaper|g00g1e.net|GrapeshotCrawler|urlappendbot|brainobot|fr-crawler|binlar|SimpleCrawler|Livelapbot|Twitterbot|cXensebot|smtbot|bnf.fr_bot|A6-Indexer|ADmantX|Facebot|Twitterbot|OrangeBot|memorybot|AdvBot|MegaIndex|SemanticScholarBot|ltx71|nerdybot|xovibot|BUbiNG|Qwantify|archive.org_bot|Applebot|TweetmemeBot|crawler4j|findxbot|SemrushBot|yoozBot|lipperhey|y!j-asr|Domain Re-Animator Bot|AddThis)/ );
 
-    // ADD "&& COUNT <= NUMBER" TO THIS END BECAUSE 100,000+ LINE FILES
-    // ================================================================
+    // ADD "IF (COUNT <= NUMBER)" TO THIS BECAUSE 100,000+ LINE FILES
+    // ==============================================================
     if (audiotypes && !bots){
         // CREATE NEW ARRAY BY SPLITTING BY SPACES, PUSH TO DATA ARRAY
         // ===========================================================
@@ -74,7 +57,24 @@ var convertEntireLogFileToJSONEliminatingHeaders = function(line){
     }
 }
 
-s = fs.createReadStream('../logs/1510.log.txt')
+var convertEntireLogFileToJSONEliminatingHeaders = function(line){
+    // ADD "IF (COUNT <= NUMBER)" TO THIS BECAUSE 100,000+ LINE FILES U KNO?
+    // =====================================================================
+    if (line.indexOf('#') == 0) {
+        // CREATE NEW ARRAY BY SPLITTING BY SPACES, PUSH TO DATA ARRAY
+        // ===========================================================
+        var arr = line.split(/[ ]+/);
+        matchCount +=1;
+        // ADD A UNIQUE ID TO EACH OBJECT, PUSHED TO THE END, MAPPING TO THE LAST KEY VALUE
+        // ================================================================================
+        arr.push('id_' + (__.uniqueId() - 1));
+        data.push(arr);
+    }
+}
+
+s = fs.createReadStream('../logs/1510.log.txt', function(){
+    console.log('yes')
+})
     .pipe(es.split())
     .pipe(es.mapSync(function(line) {
         s.pause();
@@ -82,57 +82,56 @@ s = fs.createReadStream('../logs/1510.log.txt')
         // ===================
         lineNr += 1;
 
-            // FUNCTION PER LINE --> LINE === THIS (KIND OF!)
-            // ==============================================
-            (function() {
+        // FUNCTION PER LINE --> LINE === THIS (KIND OF!)
+        // ==============================================
+        (function() {
 
-                // COUNT BECOMES FINAL LINE TOTAL, LOG WHEN FINISHED
-                // =================================================
-                count += 1;
+            // COUNT BECOMES FINAL LINE TOTAL, LOG WHEN FINISHED
+            // =================================================
+            count += 1;
 
-                // PARSE DOWN AND RETURN ONLY AUDIOFILES W/O BOTS
-                // =================================================
-                parseJSONforAudioReferences(line);
+            // PARSE DOWN AND RETURN ONLY AUDIOFILES W/O BOTS
+            // =================================================
+            parseJSONforAudioReferences(line);
 
-                // CONVERT ENTIRE FILE TO JSON ELIMINATING #HEADERS
-                // =================================================
-                // convertEntireLogFileToJSONEliminatingHeaders(line);
+            // CONVERT ENTIRE FILE TO JSON ELIMINATING #HEADERS
+            // =================================================
+            // convertEntireLogFileToJSONEliminatingHeaders(line);
 
-                s.resume();
-            })();
-        })
-        .on('error', function() {
-            console.log('Error while reading file.');
-        })
-        .on('end', function() {
+            s.resume();
+        })();
+    })
+    .on('error', function() {
+        console.log('Error while reading file.');
+    })
+    .on('end', function() {
+        // =====================================================
+        // PROTOTYPE JSON QUERY FUNCTIONS BASED ON STACKOVERFLOW
+        // =====================================================
+        // var counter = 0;
+        // var checkForValue = function(json, value) {
+        //     for (key in json) {
+        //         if (typeof(json[key]) === "object") {
+        //             return checkForValue(json[key], value);
+        //         } else if (json[key] === value) {
+        //             counter += 1;
+        //         }
+        //     }
+        //     return false;
+        // }
 
-            // =====================================================
-            // PROTOTYPE JSON QUERY FUNCTIONS BASED ON STACKOVERFLOW
-            // =====================================================
-            // var counter = 0;
-            // var checkForValue = function(json, value) {
-            //     for (key in json) {
-            //         if (typeof(json[key]) === "object") {
-            //             return checkForValue(json[key], value);
-            //         } else if (json[key] === value) {
-            //             counter += 1;
-            //         }
-            //     }
-            //     return false;
-            // }
+        // for (var i = 0; i < newOutput.length; i++) {
+        //     checkForValue(newOutput, "/podcast/sketches/179-12.mp3")
+        // }
 
-            // for (var i = 0; i < newOutput.length; i++) {
-            //     checkForValue(newOutput, "/podcast/sketches/179-12.mp3")
-            // }
+        // newOutput = JSON.stringify(__.uniq(output, false, function(item) {
+        //     return item['time'] && item['cs-uri-stem']
+        // }));
 
-            var output = rebuildArray(),
-                // newOutput = JSON.stringify(__.uniq(output, false, function(item) {
-                //     return item['time'] && item['cs-uri-stem']
-                // }));
-            newOutput = JSON.stringify(output);
+        var output = rebuildArray(),
+        newOutput = JSON.stringify(output);
 
-            console.log(matchCount)
-
-            fs.writeFileSync("../logs/tmp.json", newOutput);
-        })
+        console.log("Matched " + matchCount + " entries.")
+        fs.writeFileSync("../logs/tmp.json", newOutput);
+    })
 );

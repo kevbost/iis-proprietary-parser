708e0befa6fa57447c2eaef60baa5b92d6446399
Kevin Bost <kevbost@gmail.com>
2015-11-13 14:57:00 -0500

    pushed final changes before refactoring

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 # iis-proprietary-parser
+
+NOTE: THIS PROJECT IS A MESS AND IS DEPRECIATED, NEW ISS PARSER LOCATED AT `link to be determined`
+
 Proprietary IIS log parser creating a JSON object that matches specific criteria of audiofiles &amp;&amp; !bots.  Written in node.
 
 # why
 I got interested in the topic of parsing txt files that look like this:
+
 ```
 #Date: 2030-10-01 00:00:10
 #Fields: date time stuff interesting-stuff boring-stuf
 2030-06-02 00:00:10 stuffy-stuff superinteresting.jpg nothing-to-see-here
-2030-06-02 00:00:10 stuffidy-stuff wowthisisinterestingstuff.png nothing-to-see-here-either
-```
+2030-06-02 00:00:10 stuffidy-stuff wowthisisinterestingstuff.png nothing-to-see-here-either```
 
 A podcast I listen to was kind enough to lend me their IIS logs.
 IIS logs are massive and kind of hard to get anything useful out of as .txt files on a hard drive.  In my case, I wanted to know how many times audio files with certain keywords were downloaded by anything that is not a bot.
@@ -17,15 +20,16 @@ IIS logs are massive and kind of hard to get anything useful out of as .txt file
 
 # how
 I was able to do that with a bit of regex and a list of known webcrawlers.
+
 ```
 var audiotypes = line.match(/(\.mp3|\.m4a|\.aac|\.mp4|\.m4p|\.m4r|\.3gp|\.ogg|\.oga|\.wma|\.wav|\.flac)/);
 var bots = line.match(/(somebot|anotherbot|somebotagain)/);
-if (audiotypes && !bots) . . .  
-```
+if (audiotypes && !bots) . . .  ```
 
 If a match is found, the line is `.split(/[ ]+/);` into a new array and pushed to a global master array.
 
 After all of that is done and the new array is built, the whole thing needs to be mutated into usable key:value pairs.
+
 ```
 var data = [["key1", "key2", "key3"], ["value1", "value2", "value3"], ["value4", "value5", "value6"]];
 var mutatedData = [];
@@ -38,12 +42,11 @@ for (var i = 1; i < data.length; i++) {
   }
   mutatedData.push(bufferObject);
 }
-mutatedData;
-```
+mutatedData;```
 
 That junk works, you can paste that block into your javascript console.  An array that once looked like `[[key1, key2, key3], [value1, value2, value3], [value4, value5, value6]]` now looks like:
-```
-[
+
+```[
   {
     "key1": "value1",
     "key2": "value2",
@@ -53,8 +56,7 @@ That junk works, you can paste that block into your javascript console.  An arra
     "key2": "value5",
     "key3": "value6"
   }
-]
-```
+]```
 
 Other wise known as "magic".
 
--- app/scripts/mincer.js
+++ app/scripts/mincer.js
@@ -9,7 +9,6 @@ var fs = require('fs'),
     ansi = require('ansi'),
     cursor = ansi(process.stdout);
 
-    // cursor.bg.grey().underline()
     var hexNumberValue = 5001;
     var timer = setInterval(function () {
         var hexStringValue = '#ff' + hexNumberValue;
@@ -18,13 +17,7 @@ var fs = require('fs'),
     }, 100);
 
     simple_spinner.start(100);
-    simple_spinner.change_sequence([
-        "|...........|"
-        ,"\\.........../"
-        ,"-...........-"
-        ,"/...........\\"
-        ,"|...........|"
-    ]);
+    simple_spinner.change_sequence(["|...........|", "\\.........../", "-...........-", "/...........\\", "|...........|"]);
 
 var commandLineArguments = process.argv;
 
@@ -32,13 +25,15 @@ var $$ = {
     data: [],
     uniqueIPList: [],
     mutatedData: undefined,
-    cleanedData: undefined,
+    uniqueEpisodes: undefined,
+    extractedEpisodeList: [],
+    cleanedData: {configuration: undefined, uniqueEpisodes: undefined, episodes: undefined, matchedQuery: undefined, uniqueIPs: undefined},
     finalJSON: undefined,
     lineNr: 1,
     matchCount: 0,
     sourceFile: commandLineArguments[2],
     destinationFile: commandLineArguments[3],
-    uniqueIPListFile: [],
+    uniqueIPListFile: undefined,
     errorMessageInstructions: colors.bold('$ node <file> path/to/pathtosource.log.txt path/to/pathtodest.json\n'),
 
     checkArguments: function() {
@@ -73,10 +68,29 @@ var $$ = {
 
     extractUniqueIPs: function(line) {
         var audiotypes = line.match(/(\.mp3|\.m4a|\.aac|\.mp4|\.m4p|\.m4r|\.3gp|\.ogg|\.oga|\.wma|\.wav|\.flac)/);
-        var bots = line.match(/(googlebot|Googlebot|Googlebot-Mobile|Googlebot-Image|Mediapartners-Google|bingbot|slurp|Slurp|java|wget|curl|Commons-HttpClient|Python-urllib|libwww|httpunit|nutch|phpcrawl|msnbot|jyxobot|FAST-WebCrawler|FAST Enterprise Crawler|biglotron|teoma|convera|seekbot|gigablast|exabot|ngbot|ia_archiver|GingerCrawler|webmon|httrack|webcrawler|grub.org|UsineNouvelleCrawler|antibot|netresearchserver|speedy|fluffy|bibnum.bnf|findlink|msrbot|panscient|yacybot|AISearchBot|IOI|ips-agent|tagoobot|MJ12bot|dotbot|woriobot|yanga|buzzbot|mlbot|yandexbot|purebot|Linguee Bot|Voyager|CyberPatrol|voilabot|baiduspider|Baiduspider|citeseerxbot|spbot|twengabot|postrank|turnitinbot|scribdbot|page2rss|sitebot|linkdex|Adidxbot|blekkobot|ezooms|dotbot|Mail.RU_Bot|discobot|heritrix|findthatfile|europarchive.org|NerdByNature.Bot|sistrix crawler|ahrefsbot|Aboundex|domaincrawler|wbsearchbot|summify|ccbot|edisterbot|seznambot|ec2linkfinder|gslfbot|aihitbot|intelium_bot|facebookexternalhit|yeti|RetrevoPageAnalyzer|lb-spider|sogou|lssbot|careerbot|wotbox|wocbot|ichiro|DuckDuckBot|lssrocketcrawler|drupact|webcompanycrawler|acoonbot|openindexspider|gnam gnam spider|web-archive-net.com.bot|backlinkcrawler|coccoc|integromedb|content crawler spider|toplistbot|seokicks-robot|it2media-domain-crawler|ip-web-crawler.com|siteexplorer.info|elisabot|proximic|changedetection|blexbot|arabot|WeSEE:Search|niki-bot|CrystalSemanticsBot|rogerbot|360Spider|psbot|InterfaxScanBot|Lipperhey SEO Service|CC Metadata Scaper|g00g1e.net|GrapeshotCrawler|urlappendbot|brainobot|fr-crawler|binlar|SimpleCrawler|Livelapbot|Twitterbot|cXensebot|smtbot|bnf.fr_bot|A6-Indexer|ADmantX|Facebot|Twitterbot|OrangeBot|memorybot|AdvBot|MegaIndex|SemanticScholarBot|ltx71|nerdybot|xovibot|BUbiNG|Qwantify|archive.org_bot|Applebot|TweetmemeBot|crawler4j|findxbot|SemrushBot|yoozBot|lipperhey|y!j-asr|Domain Re-Animator Bot|AddThis)/);
+        var bots = line.match(/(php|googlebot|Googlebot|Googlebot-Mobile|Googlebot-Image|Mediapartners-Google|bingbot|slurp|Slurp|java|wget|curl|Commons-HttpClient|Python-urllib|libwww|httpunit|nutch|phpcrawl|msnbot|jyxobot|FAST-WebCrawler|FAST Enterprise Crawler|biglotron|teoma|convera|seekbot|gigablast|exabot|ngbot|ia_archiver|GingerCrawler|webmon|httrack|webcrawler|grub.org|UsineNouvelleCrawler|antibot|netresearchserver|speedy|fluffy|bibnum.bnf|findlink|msrbot|panscient|yacybot|AISearchBot|IOI|ips-agent|tagoobot|MJ12bot|dotbot|woriobot|yanga|buzzbot|mlbot|yandexbot|purebot|Linguee Bot|Voyager|CyberPatrol|voilabot|baiduspider|Baiduspider|citeseerxbot|spbot|twengabot|postrank|turnitinbot|scribdbot|page2rss|sitebot|linkdex|Adidxbot|blekkobot|ezooms|dotbot|Mail.RU_Bot|discobot|heritrix|findthatfile|europarchive.org|NerdByNature.Bot|sistrix crawler|ahrefsbot|Aboundex|domaincrawler|wbsearchbot|summify|ccbot|edisterbot|seznambot|ec2linkfinder|gslfbot|aihitbot|intelium_bot|facebookexternalhit|yeti|RetrevoPageAnalyzer|lb-spider|sogou|lssbot|careerbot|wotbox|wocbot|ichiro|DuckDuckBot|lssrocketcrawler|drupact|webcompanycrawler|acoonbot|openindexspider|gnam gnam spider|web-archive-net.com.bot|backlinkcrawler|coccoc|integromedb|content crawler spider|toplistbot|seokicks-robot|it2media-domain-crawler|ip-web-crawler.com|siteexplorer.info|elisabot|proximic|changedetection|blexbot|arabot|WeSEE:Search|niki-bot|CrystalSemanticsBot|rogerbot|360Spider|psbot|InterfaxScanBot|Lipperhey SEO Service|CC Metadata Scaper|g00g1e.net|GrapeshotCrawler|urlappendbot|brainobot|fr-crawler|binlar|SimpleCrawler|Livelapbot|Twitterbot|cXensebot|smtbot|bnf.fr_bot|A6-Indexer|ADmantX|Facebot|Twitterbot|OrangeBot|memorybot|AdvBot|MegaIndex|SemanticScholarBot|ltx71|nerdybot|xovibot|BUbiNG|Qwantify|archive.org_bot|Applebot|TweetmemeBot|crawler4j|findxbot|SemrushBot|yoozBot|lipperhey|y!j-asr|Domain Re-Animator Bot|AddThis)/);
         if (audiotypes && !bots) {
             var buff = line.split(/[ ]+/);
             $$.uniqueIPList.push(buff[8]);
+            $$.extractedEpisodeList.push(buff[4]);
+        }
+    },
+
+    pushUniqueIPsToMainObject: function(){
+    },
+
+    pushExtractedArraysToMainObject: function(){
+        $$.cleanedData.uniqueIPs = underscore.uniq($$.uniqueIPList);;;
+        $$.cleanedData.uniqueIPs = $$.uniqueIPList.sort();;
+        $$.cleanedData.episodes = $$.extractedEpisodeList.sort();
+        $$.cleanedData.uniqueEpisodes = underscore.uniq($$.extractedEpisodeList);
+
+        $$.cleanedData.configuration = {
+            'Reading From': $$.sourceFile,
+            'Wrote To': $$.destinationFile,
+            'Number of Matches to Query': $$.matchCount,
+            'Number of Unique Episodes': $$.cleanedData.uniqueEpisodes.length,
+            'Number of Unique IP Addresses': $$.uniqueIPList.length
         }
     },
 
@@ -87,7 +101,6 @@ var $$ = {
             indexofLastDecimal = url .lastIndexOf('.');
 
         $$.uniqueIPListFile = url.substring(0, indexofLastSlash + 1) + mystring + url.substring(indexofLastDecimal);
-        // console.log(new_url);
     },
 
     mutateData: function() {
@@ -112,18 +125,16 @@ var $$ = {
 
     removeDuplicatesFromMutatedData: function() {
         'use strict';
-        $$.cleanedData = underscore.uniq($$.mutatedData, false, function(item) {
+        $$.uniqueIPList = underscore.uniq($$.uniqueIPList);
+        $$.cleanedData.matchedQuery = underscore.uniq($$.mutatedData, false, function(item) {
             return item['time'] && item['cs-uri-stem'];
         });
-        $$.uniqueIPList = underscore.uniq($$.uniqueIPList);
     },
 
 
     stringifyMutatedData: function() {
         'use strict';
         $$.finalJSON = JSON.stringify($$.cleanedData);
-        $$.uniqueIPList = $$.uniqueIPList.sort();
-        $$.uniqueIPList = JSON.stringify($$.uniqueIPList);
     }
 };
 
@@ -149,9 +160,10 @@ s = fs.createReadStream($$.sourceFile)
             Promise.all(
                 [
                     $$.checkArguments(),
+                    $$.createUniqueIPFilePath(),
+                    $$.pushExtractedArraysToMainObject(),
                     $$.rebuildArray(),
                     $$.removeDuplicatesFromMutatedData(),
-                    $$.createUniqueIPFilePath(),
                     $$.stringifyMutatedData()
                 ]
             ).then(function() {
@@ -170,10 +182,6 @@ s = fs.createReadStream($$.sourceFile)
                 var _horizontalLine_ = { description: '   ------------'.gray, value: '-----------------------'.gray, filesize: '---------'.gray };
                 var data = [
                     {
-                        description: colors.green(colors.bold('\t\tIPs Wrote To')),
-                        value: colors.yellow($$.uniqueIPListFile),
-                        filesize: colors.magenta(colors.bold(ipListSize['size'] / 1000.0 + 'kb'))
-                    }, {
                         description: colors.green(colors.bold('\t\tReading From')),
                         value: colors.yellow($$.sourceFile),
                         filesize: colors.magenta(colors.bold(srcSize['size'] / 1000000.0 + 'mb'))
@@ -181,6 +189,9 @@ s = fs.createReadStream($$.sourceFile)
                         description: colors.green(colors.bold('\t\tWrote To')),
                         value: colors.yellow($$.destinationFile),
                         filesize: colors.magenta(colors.bold(destSize['size'] / 1000.0 + 'kb'))
+                    }, {
+                        description: colors.green(colors.bold('\t\tUniqueEpisodes')),
+                        value: colors.yellow($$.cleanedData.uniqueEpisodes.length)
                     }, {
                         description: colors.green(colors.bold('\t\tMatched Entries')),
                         value: colors.yellow($$.matchCount)
@@ -202,6 +213,7 @@ s = fs.createReadStream($$.sourceFile)
                 console.log('\n');
                 simple_spinner.stop();
                 clearInterval(timer);
+
             });
 
         })
